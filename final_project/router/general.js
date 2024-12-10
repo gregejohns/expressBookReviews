71c1401166a2d2a5e6a10102e19f0a3ea9e15af8
gregejohns <greg@tnergyservices.com>
2024-12-10 13:15:39 -0500

    Update and test general user routes in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,17 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  try {
+    // Send the list of books as a JSON string
+    return res.status(200).send(JSON.stringify(books, null, 2));
+  } catch (error) {
+    // Handle any potential errors
+    return res.status(500).json({ message: "Error retrieving books" });
+  }
 });
+
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
